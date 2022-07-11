ced548856cb942725c1d3021185ea83682270f46
zuhaspvt12 <99756674+zuhaspvt12@users.noreply.github.com>
2022-07-11 16:22:36 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+// main function:
+int main()
+{
+    int n,temp,position;
+    printf("Enter the number of elements you wanna enter:");
+    scanf("%d", &n);
+    int array[n], data;
 
+    // Entering data to array:
+    printf("Enter data to sort:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter: ");
+        scanf("%d", &data);
+        array[i] = data;
+    }
+
+    // selection_sort algorithm:
+   for (int i = 0; i < n - 1 ; i++) 
+   {
+      int pos = array[i];
+      int j = i-1;
+      while (pos<array[j] && j>=0)
+      {
+        array[j+1]=array[j];
+        --j;
+      }
+      array[j+1]=pos;
+      
+   }
+
+    // printing final array:
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+}
